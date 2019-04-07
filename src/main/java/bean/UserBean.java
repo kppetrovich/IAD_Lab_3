bdc3783bce1c8c8e3b88ec12155d4fabda066c8c
Pavel <paulkur24@gmail.com>
2019-04-07 23:02:46 +0300

    feat: add svg picture

--- src/main/java/bean/UserBean.java
+++ src/main/java/bean/UserBean.java
@@ -11,7 +11,17 @@ import java.util.Map;
 @ManagedBean("userBean")
 @ApplicationScoped
 public class UserBean implements Serializable {
+
     private Map<String, User> usersMap = new HashMap<>();
+    private boolean isAuthFlag = true;
+
+    public boolean isAuthFlag() {
+        return this.isAuthFlag;
+    }
+
+    public void revertAuthFlag() {
+        this.isAuthFlag = !this.isAuthFlag;
+    }
 
     public Map<String, User> getUsersMap() {
         return usersMap;
